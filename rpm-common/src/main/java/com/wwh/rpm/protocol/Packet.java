7f8e2a8f48f0ba8485d03f05030da8b1abae4e8b
wwh <wangwen135@163.com>
2020-08-07 21:10:48 +0800

    XX

--- rpm-common/src/main/java/com/wwh/rpm/protocol/Packet.java
+++ rpm-common/src/main/java/com/wwh/rpm/protocol/Packet.java
@@ -2,6 +2,12 @@ package com.wwh.rpm.protocol;
 
 public class Packet {
 
+    private byte type;
+
+    private int length;
+
+    private byte[] body;
+
     public Packet() {
     }
 
---
+++ rpm-common/src/main/java/com/wwh/rpm/protocol/codec/PacketDecoder.java
@@ -0,0 +1,5 @@
+package com.wwh.rpm.protocol.codec;
+
+public class PacketDecoder {
+
+}
