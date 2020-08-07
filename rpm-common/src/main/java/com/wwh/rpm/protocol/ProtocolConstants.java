9b6e56d8360c9198a7bce90629ec7135677ee1e6
wangwenhua <wangwh@vvsmart.com>
2020-08-07 21:10:48 +0800

    XX

---
+++ rpm-common/src/main/java/com/wwh/rpm/protocol/ProtocolConstants.java
@@ -0,0 +1,16 @@
+package com.wwh.rpm.protocol;
+
+/**
+ * 协议常量
+ * 
+ * @author wwh
+ *
+ */
+public class ProtocolConstants {
+
+    /**
+     * 魔法数字
+     */
+    public static final byte MAGIC_NUMBER = (byte) 0x88;
+    
+}
